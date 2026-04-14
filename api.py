7c077a415c9d40b420a82582d5be5e7d7d859958
Timz-creator <timarleyf@gmail.com>
2026-04-14 13:24:27 +0100

    feat: added CORS middleware and inpute validation to API

--- api.py
+++ api.py
@@ -2,14 +2,22 @@ from fastapi import FastAPI
 from athena.scenario import ScenarioRunner
 from athena.monte_carlo import MonteCarlo
 from athena.doe import DesignOfExperiments
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class ScenarioRequest(BaseModel):
-    width: int
-    height: int
-    n_agents_per_team: int
-    steps: int
+    width: int = Field(gt=0, le=1000)
+    height: int = Field(gt=0, le=1000)
+    n_agents_per_team: int = Field(gt=0, le=20)
+    steps: int = Field(gt=0, le=1000)
 
 @app.post("/scenario/run")
 def run_scenario(request: ScenarioRequest):
@@ -22,11 +30,11 @@ def run_scenario(request: ScenarioRequest):
     return runner.get_results()
 
 class MonteCarloRequest(BaseModel):
-    width: int
-    height: int
-    n_agents_per_team: int
-    steps: int
-    n_runs: int
+    width: int =Field(gt=0, le=1000)
+    height: int =Field(gt=0, le=1000)
+    n_agents_per_team: int=Field(gt=0, le=20)
+    steps: int=Field(gt=0, le=1000)
+    n_runs: int=Field(gt=0, le=1000)
 
 @app.post("/monte-carlo/run")
 def run_monteCarlo(request: MonteCarloRequest):

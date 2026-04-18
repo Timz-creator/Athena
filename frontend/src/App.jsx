0967874c8c95529d42564db79f37caa2a0e04b8f
Timz-creator <timarleyf@gmail.com>
2026-04-18 19:51:44 +0100

    feat: created frontend folder, installed react and started component creation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="flex h-screen w-screen bg-[#080d14] overflow-hidden"></div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+function Sidebar() {
+  return (
+    <aside className="flex flex-col items-center py-4 w-[280px] h-screen fixed left-0 top-0 z-40 bg-[#0e0e0e] border-r border-[#1a1a1a]"></aside>
+  );
+}
+
+export default Sidebar;
